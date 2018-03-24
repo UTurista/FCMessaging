cb0a24cecbdb0f6b18fec2575ceaf7f71be1c02f
UTurista <vasckko@gmail.com>
2018-03-24 02:23:21 +0000

    Added integration tests for the FcmClient

---
+++ FCMessagingTests/src/FcmClientTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UTurista.FCMessaging;
+
+namespace FCMessagingTests.src
+{
+    [TestClass]
+    public class FcmClientTests
+    {
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException),
+            "FCMClient must throw error if credential files is inexistente")]
+        public void LoadWrongCredentials_NoArguments()
+        {
+            FCMClient client = new FCMClient();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException),
+            "FCMClient must throw error if credential files is inexistente")]
+        public void LoadWrongCredentials_BadArguments()
+        {
+            FCMClient client = new FCMClient("a_bad_file.json");
+        }
+
+        [TestMethod]
+        public void LoadCorrectCredentials()
+        {
+            FCMClient client = new FCMClient("credentials.json");
+        }
+
+
+        [TestMethod]
+        public void SimpleSend()
+        {
+            FCMClient client = new FCMClient("credentials.json");
+
+            var message = new Message
+                .Builder()
+                .Title("")
+                .ToTopic("aTopic")
+                .Build();
+
+            // Lets send it
+            string id = client.Send(message, true).GetAwaiter().GetResult();
+
+            Assert.IsNotNull(id);
+        }
+    }
+}
